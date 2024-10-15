1edf17f399558bdb684ee7ddfab20f49768984d1
Shoichi Kaji <skaji@cpan.org>
2024-10-16 05:42:08 +0900

    update

--- lib/attributes/EXPORT.pm
+++ lib/attributes/EXPORT.pm
@@ -2,36 +2,34 @@ package attributes::EXPORT;
 use v5.40;
 use experimental qw(builtin class defer);
 
+use meta;
+no warnings 'meta::experimental';
+
 my %export;
-my $modify_attributes = sub ($, $v, @attr) {
-    my @attr2 = grep { $_ ne 'EXPORT' } @attr;
-    if (@attr == @attr2) {
-        return @attr;
-    }
-    $export{refaddr $v} = true;
-    return @attr2;
-};
 
 sub import ($) {
+    my sub modify_attributes ($, $v, @attr) {
+        my @attr2 = grep { $_ ne 'EXPORT' } @attr;
+        if (@attr == @attr2) {
+            return @attr;
+        }
+        $export{refaddr $v} = true;
+        return @attr2;
+    }
+
     my $caller = caller;
-    no strict 'refs';
-    *{ $caller . "::MODIFY_CODE_ATTRIBUTES" } = $modify_attributes;
-    *{ $caller . "::MODIFY_SCALAR_ATTRIBUTES" } = $modify_attributes;
+    my $meta = meta::get_package($caller);
+    $meta->add_symbol('&MODIFY_CODE_ATTRIBUTES' => \&modify_attributes);
+    $meta->add_symbol('&MODIFY_SCALAR_ATTRIBUTES' => \&modify_attributes);
 }
 
 sub get_symbols ($, $package) {
+    my $meta = meta::get_package($package);
     my %symbol;
-    for my ($name, $v) (do { no strict 'refs'; %{ $package . "::" } }) {
-        next if ref(\$v) ne 'GLOB';
-        if (my $code = *{$v}{CODE}) {
-            if ($export{refaddr $code}) {
-                $symbol{ $name } = $code;
-            }
-        }
-        if (my $scalar = *{$v}{SCALAR}) {
-            if ($export{refaddr $scalar}) {
-                $symbol{ '$' . $name } = $scalar;
-            }
+    for my ($name, $symbol) ($meta->list_symbols) {
+        my $ref = $symbol->reference;
+        if ($export{refaddr $ref}) {
+            $symbol{$name} = $ref;
         }
     }
     \%symbol;
